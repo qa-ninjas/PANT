ee66c061882dfd9b6cd571c4387783ce7779b26b
patmccler <patmccler@gmail.com>
2018-12-05 18:00:35 -0500

    removed old comments from Login

--- workflows/login.rb
+++ workflows/login.rb
@@ -2,15 +2,7 @@
 require_relative "./workflow.rb"
 
 class Login < Workflow
-    #probably implement a test-superclass, or use a framework for that?
-
-
-    # for example, this type of init can likely be standardized?
-    # maybe not, if we want to do multi-user tests?
-    # better left as two separate tests run in series?
-    # could be reasonable that any one test must be one user only
-    # and we could come up with a clever way to string em together.
-
+    
     def run
         super
         @user.login
